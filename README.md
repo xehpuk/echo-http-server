65a9c846d8a883d3e21d604391772547fce87c55
xehpuk <github@xehpuk.com>
2021-07-10 00:29:36 +0200

    First!

---
+++ README.md
@@ -0,0 +1,24 @@
+# EchoHttpServer
+
+This HTTP server does the following:
+
+1. Echoes the request headers, potentially prefixed and suffixed.
+2. Responds to the request, depending on the method:
+   1. `PATCH`, `POST`, `PUT`: Pipes the request body to the response body.
+   2. `DELETE`, `HEAD`, `GET`: Empty 200 OK
+   3. `OPTIONS`: Behaves like a normal server by returning the allowed headers.
+   4. else: 405 Method Not Allowed
+
+```
+Usage: echo-http-server [-?vV] [-b=<backlog>] [-h=<host>] [-p=<port>] [-P=<headerPrefix>] [-s=<headerSuffix>]
+  -?, --help                display this help message
+  -b, --backlog=<backlog>   the maximum number of queued incoming connections to allow (default 1)
+  -h, --host=<host>         the host to listen on (default "localhost")
+  -p, --port=<port>         the port to listen on (default 8080)
+  -P, --prefix=<headerPrefix>
+                            the prefix to use for the echoed headers (default "X-Echo-")
+  -s, --suffix=<headerSuffix>
+                            the suffix to use for the echoed headers
+  -v, --verbose             log incoming requests
+  -V, --version             display version info
+```
---
+++ src/main/java/de/xehpuk/echo_http_server/EchoHttpServer.java
@@ -0,0 +1,173 @@
+package de.xehpuk.echo_http_server;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UncheckedIOException;
+
+import java.net.InetSocketAddress;
+
+import java.time.LocalDateTime;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Stream;
+
+@Command(name = "echo-http-server", version = "EchoHttpServer 1.0.0")
+public class EchoHttpServer implements Runnable {
+	@Option(names = {"-V", "--version"}, versionHelp = true, description = "display version info")
+	private boolean versionInfoRequested;
+
+	@Option(names = {"-?", "--help"}, usageHelp = true, description = "display this help message")
+	private boolean usageHelpRequested;
+
+	@Option(names = {"-h", "--host"}, description = "the host to listen on (default \"${DEFAULT-VALUE}\")")
+	private String host = "localhost";
+
+	@Option(names = {"-p", "--port"}, description = "the port to listen on (default ${DEFAULT-VALUE})")
+	private int port = 8080;
+	
+	@Option(names = {"-P", "--prefix"}, description = "the prefix to use for the echoed headers (default \"${DEFAULT-VALUE}\")")
+	private String headerPrefix = "X-Echo-";
+	
+	@Option(names = {"-s", "--suffix"}, description = "the suffix to use for the echoed headers")
+	private String headerSuffix = "";
+
+	@Option(names = {"-b", "--backlog"}, description = "the maximum number of queued incoming connections to allow (default ${DEFAULT-VALUE})")
+	private int backlog = 1;
+
+	@Option(names = {"-v", "--verbose"}, description = "log incoming requests")
+	private boolean verbose;
+
+	private static class Header {
+		static final String ALLOW = "Allow";
+		static final String CONTENT_LENGTH = "Content-Length";
+		static final String CONTENT_TYPE = "Content-Type";
+	}
+
+	private static class Status {
+		static final int OK = 200;
+		static final int METHOD_NOT_ALLOWED = 405;
+	}
+
+	private static class ResponseLength {
+		static final int NONE = -1;
+		static final int CHUNKED = 0;
+	}
+
+	private static class Method {
+		static final String DELETE = "DELETE";
+		static final String GET = "GET";
+		static final String HEAD = "HEAD";
+		static final String OPTIONS = "OPTIONS";
+		static final String PATCH = "PATCH";
+		static final String POST = "POST";
+		static final String PUT = "PUT";
+	}
+
+	private static final String ALLOWED_METHODS = String.join(",",
+		Method.DELETE,
+		Method.GET,
+		Method.HEAD,
+		Method.OPTIONS,
+		Method.PATCH,
+		Method.POST,
+		Method.PUT
+	);
+	
+	static {
+		System.setProperty("picocli.usage.width", "120");
+	}
+
+	public static void main(final String... args) throws IOException {
+		final int exitCode = new CommandLine(new EchoHttpServer()).execute(args);
+		if (exitCode != 0) {
+			System.exit(exitCode);
+		}
+	}
+
+	private Stream<Entry<String, List<String>>> streamHeaders(final Map<String, List<String>> map) {
+		final var stream = map.entrySet().stream();
+		return verbose
+			? stream.peek(header -> header.getValue().forEach(value -> System.out.printf("%s: %s%n", header.getKey(), value)))
+			: stream;
+	}
+
+	private void beforeRequest(final HttpExchange he) {
+		if (verbose) {
+			System.out.printf("[%1$tF %1$tT] %2$s %3$s %4$s%n",
+				LocalDateTime.now(),
+				he.getRequestMethod(),
+				he.getRequestURI(),
+				he.getProtocol());
+		}
+	}
+
+	private void afterRequest(final HttpExchange he) {
+		if (verbose) {
+			System.out.println();
+		}
+	}
+
+	@Override
+	public void run() {
+		final HttpServer server;
+		try {
+			server = HttpServer.create(new InetSocketAddress(host, port), backlog);
+		} catch (final IOException ioe) {
+			throw new UncheckedIOException(ioe);
+		}
+
+		server.createContext("/", he -> {
+			try (he) {
+				final Headers reqHeaders = he.getRequestHeaders();
+				final Headers resHeaders = he.getResponseHeaders();
+				beforeRequest(he);
+				streamHeaders(reqHeaders)
+					.map(entry -> Map.entry(headerPrefix + entry.getKey() + headerSuffix, entry.getValue()))
+					.forEach(entry -> resHeaders.put(entry.getKey(), entry.getValue()));
+				afterRequest(he);
+				if (reqHeaders.containsKey(Header.CONTENT_TYPE)) {
+					resHeaders.set(Header.CONTENT_TYPE, reqHeaders.getFirst(Header.CONTENT_TYPE));
+				}
+
+				switch (he.getRequestMethod()) {
+					case Method.PATCH, Method.POST, Method.PUT -> {
+						he.sendResponseHeaders(Status.OK, reqHeaders.containsKey(Header.CONTENT_LENGTH)
+							? Long.parseLong(reqHeaders.getFirst(Header.CONTENT_LENGTH))
+							: ResponseLength.CHUNKED);
+						try (
+							InputStream is = he.getRequestBody();
+							OutputStream os = he.getResponseBody();
+						) {
+							is.transferTo(os);
+						}
+					}
+					case Method.DELETE, Method.HEAD, Method.GET ->
+						he.sendResponseHeaders(Status.OK, ResponseLength.NONE);
+					case Method.OPTIONS -> {
+						resHeaders.set(Header.ALLOW, ALLOWED_METHODS);
+						he.sendResponseHeaders(Status.OK, ResponseLength.NONE);
+					}
+					default -> {
+						resHeaders.set(Header.ALLOW, ALLOWED_METHODS);
+						he.sendResponseHeaders(Status.METHOD_NOT_ALLOWED, ResponseLength.NONE);
+					}
+				}
+			}
+		});
+
+		server.start();
+
+		System.out.println("EchoHttpServer running at " + server.getAddress());
+	}
+}
